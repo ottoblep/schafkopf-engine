a247ec876ca46e5a9305bee9ca7a8b5751f6deb5
Severin Lochschmidt <seviron53@gmail.com>
2024-03-26 17:43:37 +0100

    Remove main function for now

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,4 @@ use crate::table::rulesets::*;
 use crate::table::Game;
 
 fn main() {
-    let mut game = Game::new(0);
-    game.set_ruleset(SAUSPIEL);
-    loop {
-        let hand_on_turn = game.get_cards_in_location(game.vorhand+1);
-        for card in hand_on_turn {
-            if game.play_card(&card).unwrap() == true { break; }
-        }
-    }
 }
