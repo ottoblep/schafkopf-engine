6e17a46907a2b0a52dc592b989670d89f08d93b5
Severin Lochschmidt <seviron53@gmail.com>
2024-03-30 14:41:16 +0100

    Format state

--- src/table/state.rs
+++ src/table/state.rs
@@ -1,4 +1,3 @@
-
 pub struct GameState {
     pub state: u8,
     // 0. Start
@@ -12,39 +11,39 @@ impl GameState {
     }
     pub fn AnnounceNone(&mut self) {
         match self.state {
-            0 => { self.state = 1 },
-            1 => { self.state = 2 },
-            2 => { self.state = 3 },
-            3 => { self.state = 4 },
-            _ => ()
+            0 => self.state = 1,
+            1 => self.state = 2,
+            2 => self.state = 3,
+            3 => self.state = 4,
+            _ => (),
         }
     }
     pub fn AnnounceSome(&mut self) {
         match self.state {
-            0 => { self.state = 1 },
-            2 => { self.state = 1 },
-            3 => { self.state = 1 },
-            _ => ()
+            0 => self.state = 1,
+            2 => self.state = 1,
+            3 => self.state = 1,
+            _ => (),
         }
     }
     pub fn PlayCard(&mut self) {
         match self.state {
-            4 => { self.state = 5 },
-            5 => { self.state = 6 },
-            6 => { self.state = 7 },
-            _ => ()
+            4 => self.state = 5,
+            5 => self.state = 6,
+            6 => self.state = 7,
+            _ => (),
         }
     }
     pub fn NextRound(&mut self) {
         match self.state {
-            7 => { self.state = 4 },
-            _ => ()
+            7 => self.state = 4,
+            _ => (),
         }
     }
     pub fn Finish(&mut self) {
         match self.state {
-            7 => { self.state = 8 },
-            _ => ()
+            7 => self.state = 8,
+            _ => (),
         }
     }
 }
